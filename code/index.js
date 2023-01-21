4817a8660beb8d5c36212b49a117fdd51486d399
CathGlover <117387100+CathGlover@users.noreply.github.com>
2023-01-21 10:13:47 +0000

    added Set Interval

--- code/index.js
+++ code/index.js
@@ -1,35 +1,39 @@
-let locationOne = document.querySelector(".location-1");
+setInterval(function () {
+  let locationOne = document.querySelector(".location-1");
 
-let locationOneDate = document.querySelector(".date-1");
+  let locationOneDate = document.querySelector(".date-1");
 
-let locationOneTime = document.querySelector(".time-1");
+  let locationOneTime = document.querySelector(".time-1");
 
-let locationOneAmPm = document.querySelector(".ampm-1");
+  let locationOneAmPm = document.querySelector(".ampm-1");
 
-let locationOneLiveDate = moment()
-  .tz("America/Los_Angeles")
-  .format("dddd Do YYYY");
+  let locationOneLiveDate = moment()
+    .tz("America/Los_Angeles")
+    .format("dddd Do YYYY");
 
-locationOneDate.innerHTML = locationOneLiveDate;
+  locationOneDate.innerHTML = locationOneLiveDate;
 
-let locationOneLiveTime = moment()
-  .tz("America/Los_Angeles")
-  .format("HH:mm:ss [<small>]A[</small>]");
+  let locationOneLiveTime = moment()
+    .tz("America/Los_Angeles")
+    .format("HH:mm:ss [<small>]A[</small>]");
 
-locationOneTime.innerHTML = locationOneLiveTime;
+  locationOneTime.innerHTML = locationOneLiveTime;
+}, 1000);
 
-let locationTwo = document.querySelector(".location-2");
+setInterval(function () {
+  let locationTwo = document.querySelector(".location-2");
 
-let locationTwoDate = document.querySelector(".date-2");
+  let locationTwoDate = document.querySelector(".date-2");
 
-let locationTwoTime = document.querySelector(".time-2");
+  let locationTwoTime = document.querySelector(".time-2");
 
-let locationTwoLiveDate = moment().tz("Europe/Paris").format("dddd Do YYYY");
+  let locationTwoLiveDate = moment().tz("Europe/Paris").format("dddd Do YYYY");
 
-locationTwoDate.innerHTML = locationTwoLiveDate;
+  locationTwoDate.innerHTML = locationTwoLiveDate;
 
-let locationTwoLiveTime = moment()
-  .tz("America/Los_Angeles")
-  .format("HH:mm:ss [<small>]A[</small>]");
+  let locationTwoLiveTime = moment()
+    .tz("Europe/Paris")
+    .format("HH:mm:ss [<small>]A[</small>]");
 
-locationTwoTime.innerHTML = locationTwoLiveTime;
+  locationTwoTime.innerHTML = locationTwoLiveTime;
+}, 1000);
